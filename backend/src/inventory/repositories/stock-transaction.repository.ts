ea21a63d8c91a96a62facb07e38282d2f702bc8f
Yusuf Falade <faladeyusuf54@gmail.com>
2025-06-27 09:19:12 +0100

    codebase clean up

--- backend/src/inventory/repositories/stock-transaction.repository.ts
+++
@@ -1,5 +0,0 @@
-import { EntityRepository, Repository } from "typeorm"
-import { StockTransaction } from "../entities/stock-transaction.entity"
-
-@EntityRepository(StockTransaction)
-export class StockTransactionRepository extends Repository<StockTransaction> {}
--- backend/src/scheduled-tasks/dto/update-scheduled-task.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from "@nestjs/mapped-types"
-import { CreateScheduledTaskDto } from "./create-scheduled-task.dto"
-
-export class UpdateScheduledTaskDto extends PartialType(CreateScheduledTaskDto) {}
