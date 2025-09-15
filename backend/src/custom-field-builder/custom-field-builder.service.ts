08de1864aa8323c30c313f220475ff122252d8b9
KAMALDEEN333 <kamalaliyu503@gmail.com>
2025-09-15 10:33:33 +0100

    updated custom service

--- backend/src/custom-field-builder/custom-field-builder.service.ts
+++ backend/src/custom-field-builder/custom-field-builder.service.ts
@@ -21,4 +21,51 @@ export class CustomFieldBuilderService {
   listForms(): CustomForm[] {
     return this.forms;
   }
+
+  updateForm(id: number, name?: string): CustomForm | undefined {
+    const form = this.getForm(id);
+    if (form && name) {
+      form.name = name;
+    }
+    return form;
+  }
+
+  deleteForm(id: number): boolean {
+    const idx = this.forms.findIndex(f => f.id === id);
+    if (idx !== -1) {
+      this.forms.splice(idx, 1);
+      return true;
+    }
+    return false;
+  }
+
+  addField(formId: number, field: { name: string; type: CustomFieldType; options?: string[] }): CustomField | undefined {
+    const form = this.getForm(formId);
+    if (!form) return undefined;
+    const newField = new CustomField(this.nextFieldId++, field.name, field.type, field.options);
+    form.fields.push(newField);
+    return newField;
+  }
+
+  updateField(formId: number, fieldId: number, data: { name?: string; type?: CustomFieldType; options?: string[] }): CustomField | undefined {
+    const form = this.getForm(formId);
+    if (!form) return undefined;
+    const field = form.fields.find(f => f.id === fieldId);
+    if (!field) return undefined;
+    if (data.name) field.name = data.name;
+    if (data.type) field.type = data.type;
+    if (data.options) field.options = data.options;
+    return field;
+  }
+
+  deleteField(formId: number, fieldId: number): boolean {
+    const form = this.getForm(formId);
+    if (!form) return false;
+    const idx = form.fields.findIndex(f => f.id === fieldId);
+    if (idx !== -1) {
+      form.fields.splice(idx, 1);
+      return true;
+    }
+    return false;
+  }
 }
